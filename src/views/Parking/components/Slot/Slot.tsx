2783a3f5638a00e3abde3b6e4bd4cbf7b899ddf7
jojo Casidsid <jvcasidsid2013@gmail.com>
2022-05-07 20:21:40 +0800

    adjust slot style

--- src/views/Parking/components/Slot/Slot.tsx
+++ src/views/Parking/components/Slot/Slot.tsx
@@ -6,9 +6,11 @@ import pluralize from 'pluralize'
 
 import numbertToType from 'helpers/numberToType/numberToType'
 
+import Card from 'components/Card'
+
 import { IProps } from './types'
 
-import { ThumbnailWrapper, StyledImage, StyledSlot } from './styles'
+import { StyledImage, StyledBody } from './styles'
 
 const Slot = ({ data, handleLeave }: IProps) => {
 	const [timeDiff, setTimeDiff] = useState(0)
@@ -24,63 +26,64 @@ const Slot = ({ data, handleLeave }: IProps) => {
 	}, [data])
 
 	return (
-		<ThumbnailWrapper>
-			<StyledSlot>
-				<Typography variant='h1'>{data.id}</Typography>
-
-				<StyledImage color={data.parkedType ? 'primary' : 'secondary'} />
-			</StyledSlot>
-
-			{data.parkedType !== 0 && (
-				<Typography variant='h4'>Plate: {data.vehicle}</Typography>
-			)}
-
-			<Typography variant='h3'>
-				Slot:{' '}
-				{data.parkedType
-					? `${numbertToType(data.parkedType)} vehicle is parked`
-					: 'Available'}
-			</Typography>
-			<Typography variant='h3'>
-				Parking Type: {numbertToType(data.type)} <br />
-			</Typography>
-			<br />
-
-			<Typography variant='h4'>
-				Distances: [ {pluralize('unit', data.west, true)} ,{' '}
-				{pluralize('unit', data.east, true)} , {pluralize('unit', data.south, true)}{' '}
-				]{' '}
-			</Typography>
-
-			<br />
-			{data.parkedType ? (
-				<Typography variant='h4'>
-					{pluralize('Hour', timeDiff, true)} /{' '}
-					{moment(data.parkTime).format('YYYY-MM-DD HH:mm:ss')}{' '}
-				</Typography>
-			) : (
-				<div />
-			)}
-
-			<br />
-			{data.parkedType && !data.unparkTime ? (
-				<Button variant='contained' fullWidth onClick={() => handleLeave(data.id)}>
-					<Typography variant='h4' color='white'>
-						Leave
+		<Card
+			header={
+				<>
+					<Typography variant='h1'>{data.id}</Typography>
+					<StyledImage color={data.parkedType ? 'primary' : 'secondary'} />
+				</>
+			}
+		>
+			<StyledBody>
+				{data.parkedType !== 0 ? (
+					<Typography variant='h4'>
+						Plate: {data.vehicle} || Type: {`${numbertToType(data.parkedType)}`}
 					</Typography>
-				</Button>
-			) : (
-				<div />
-			)}
+				) : (
+					<Typography variant='h3'>Available</Typography>
+				)}
 
-			{data.unparkTime ? (
-				<Typography variant='h4'>
-					Unpark Time: {moment(data.unparkTime).format('YYYY-MM-DD HH:mm:ss')}{' '}
+				<Typography variant='h3'>
+					Parking Type: {numbertToType(data.type)} <br />
 				</Typography>
-			) : (
-				<div />
-			)}
-		</ThumbnailWrapper>
+				<br />
+
+				<Typography variant='h4'>
+					Distances: [ {pluralize('unit', data.west, true)} ,{' '}
+					{pluralize('unit', data.east, true)} ,{' '}
+					{pluralize('unit', data.south, true)} ]{' '}
+				</Typography>
+
+				<br />
+				{data.parkedType ? (
+					<Typography variant='h4'>
+						{pluralize('Hour', timeDiff, true)} /{' '}
+						{moment(data.parkTime).format('YYYY-MM-DD HH:mm:ss')}{' '}
+					</Typography>
+				) : (
+					<div />
+				)}
+
+				<br />
+				{data.parkedType && !data.unparkTime ? (
+					<Button variant='contained' fullWidth onClick={() => handleLeave(data.id)}>
+						<Typography variant='h4' color='white'>
+							Leave
+						</Typography>
+					</Button>
+				) : (
+					<div />
+				)}
+
+				{data.unparkTime ? (
+					<Typography variant='h4'>
+						Unpark Time: {moment(data.unparkTime).format('YYYY-MM-DD HH:mm:ss')}{' '}
+					</Typography>
+				) : (
+					<div />
+				)}
+			</StyledBody>
+		</Card>
 	)
 }
 
--- src/views/Parking/components/Slot/styles.ts
+++ src/views/Parking/components/Slot/styles.ts
@@ -1,15 +1,6 @@
 import styled from '@emotion/styled'
 import DirectionsCarIcon from '@mui/icons-material/DirectionsCar'
 
-export const ThumbnailWrapper = styled('div')`
-	display: flex;
-	justify-content: flex-start;
-	align-items: center;
-	padding: 20px;
-	flex-direction: column;
-	border-radius: 30px;
-	border: 2px solid ${({ theme }) => theme.palette.secondary.main};
-`
 export const StyledSlot = styled('div')`
 	display: flex;
 	justify-content: space-around;
@@ -18,4 +9,12 @@ export const StyledSlot = styled('div')`
 
 export const StyledImage = styled(DirectionsCarIcon)`
 	font-size: 45px;
+`
+
+export const StyledBody = styled('div')`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	flex-direction: column;
+	text-align: center;
 `
